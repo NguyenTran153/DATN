855c43a823577b5873121fef97e9adce2f8032e3
MarsonVo2002 <Leovo2002@gmail.com>
2024-04-13 16:34:16 +0700

    Fix color of login screen and sig up screen. Build profile screen

--- source/screens/LoginScreen.tsx
+++ source/screens/LoginScreen.tsx
@@ -10,17 +10,103 @@ import {
   Alert,
 } from 'react-native';
 import React, {useState} from 'react';
+import {useTheme} from 'react-native-paper';
 const account = {
   email: 'myaccount@gmail.com',
   password: '123456789',
 };
+
 const LoginScreen = ({navigation}: {navigation: any}) => {
+  const theme = useTheme();
   const [form, setForm] = useState({
     email: '',
     password: '',
   });
+  const styles = StyleSheet.create({
+    container: {
+      padding: 24,
+      flex: 1,
+    },
+    header: {
+      marginVertical: 36,
+    },
+    img: {
+      width: 100,
+      height: 100,
+      alignSelf: 'center',
+      borderRadius: 10,
+      marginBottom: 36,
+    },
+    title: {
+      fontSize: 27,
+      fontWeight: '700',
+      color: theme.colors.primary,
+      marginBottom: 6,
+      textAlign: 'center',
+    },
+    input: {
+      marginBottom: 16,
+    },
+    inputLabel: {
+      fontSize: 17,
+      fontWeight: '600',
+      color: theme.colors.primary,
+      marginBottom: 8,
+    },
+    inputControl: {
+      borderColor: theme.colors.tertiary,
+      height: 44,
+      backgroundColor: theme.colors.background,
+      paddingHorizontal: 16,
+      borderRadius: 12,
+      fontSize: 15,
+      fontWeight: '500',
+      color: theme.colors.tertiary,
+      borderLeftWidth: 1,
+      borderRightWidth: 1,
+      borderBottomWidth: 1,
+      borderTopWidth: 1,
+    },
+    subtitle: {
+      fontSize: 15,
+      fontWeight: '500',
+      color: theme.colors.secondary,
+      textAlign: 'center',
+    },
+    form: {
+      marginBottom: 24,
+      flex: 1,
+    },
+    formAction: {
+      marginVertical: 24,
+    },
+    formFooter: {
+      fontSize: 17,
+      fontWeight: '600',
+      color: theme.colors.secondary,
+      textAlign: 'center',
+      letterSpacing: 0.15,
+    },
+    btn: {
+      backgroundColor: theme.colors.primary,
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: theme.colors.primary,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+    },
+    btnText: {
+      fontSize: 18,
+      fontWeight: '600',
+      color: theme.colors.background,
+    },
+  });
+  
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#eBecf4'}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: theme.colors.background}}>
       <View style={styles.container}>
         <View style={styles.header}>
           <Image
@@ -42,7 +128,7 @@ const LoginScreen = ({navigation}: {navigation: any}) => {
               keyboardType="email-address"
               value={form.email}
               placeholder="email@gmail.com"
-              placeholderTextColor="#6b7280"
+              placeholderTextColor={theme.colors.secondary}
               onChangeText={email => setForm({...form, email})}
             />
           </View>
@@ -54,11 +140,11 @@ const LoginScreen = ({navigation}: {navigation: any}) => {
               secureTextEntry
               value={form.password}
               placeholder="Password"
-              placeholderTextColor="#6b7280"
+              placeholderTextColor={theme.colors.secondary}
               onChangeText={password => setForm({...form, password})}
             />
             <TouchableOpacity style={{marginTop: 'auto'}} onPress={() => {}}>
-              <Text style={{textDecorationLine: 'underline', color: '#3349FF'}}>
+              <Text style={{textDecorationLine: 'underline', color: theme.colors.primary}}>
                 Forgot password
               </Text>
             </TouchableOpacity>
@@ -102,88 +188,3 @@ const LoginScreen = ({navigation}: {navigation: any}) => {
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-    flex: 1,
-  },
-  header: {
-    marginVertical: 36,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    alignSelf: 'center',
-    borderRadius: 10,
-    marginBottom: 36,
-  },
-  title: {
-    fontSize: 27,
-    fontWeight: '700',
-    color: '#1e1e1e',
-    marginBottom: 6,
-    textAlign: 'center',
-  },
-  input: {
-    marginBottom: 16,
-  },
-  inputLabel: {
-    fontSize: 17,
-    fontWeight: '600',
-    color: '#222',
-    marginBottom: 8,
-  },
-  inputFocused: {
-    borderColor: 'blue',
-  },
-  inputControl: {
-    borderColor: 'black',
-    height: 44,
-    backgroundColor: '#fff',
-    paddingHorizontal: 16,
-    borderRadius: 12,
-    fontSize: 15,
-    fontWeight: '500',
-    color: '#222',
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
-    borderTopWidth: 1,
-  },
-  subtitle: {
-    fontSize: 15,
-    fontWeight: '500',
-    color: '#929292',
-    textAlign: 'center',
-  },
-  form: {
-    marginBottom: 24,
-    flex: 1,
-  },
-  formAction: {
-    marginVertical: 24,
-  },
-  formFooter: {
-    fontSize: 17,
-    fontWeight: '600',
-    color: '#222',
-    textAlign: 'center',
-    letterSpacing: 0.15,
-  },
-  btn: {
-    backgroundColor: '#075eec',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#075eec',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
-  btnText: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#fff',
-  },
-});
--- source/screens/ProfileScreen.tsx
+++ source/screens/ProfileScreen.tsx
@@ -1,14 +1,114 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-
+import { Text, Button, List } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View, Image, ScrollView } from 'react-native';
 const ProfileScreen = () => {
+  
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          source={require('../asset/7677205.jpg')}
+          style={styles.img}
+        />
+        <View style={styles.patientInfo}>
+          <View style={styles.editContainer}>
+            <Text style={styles.patientName}>Patient name</Text>
+            <Button icon='pencil' mode='text' onPress={() => { }} contentStyle={{ flexDirection: 'row-reverse' }}>edit</Button>
+          </View>
+          <Text>Date of birth: 1991/02/01</Text>
+          <Text>Gender: Female</Text>
+          <Text>Address: 68894 Caleigh Trafficway Suite 387</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.settingCenter}>
+        <List.Section>
+          <List.Item
+            title="Visit History"
+            description="Patient's recent visits with a healthcare provider"
+            left={() => <List.Icon icon="hospital-building" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+          <List.Item
+            title="Medical History"
+            description="Patient's medical history"
+            left={() => <List.Icon icon="stethoscope" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+          <List.Item
+            title="Complaint"
+            description="Patient complaint or medical concern"
+            left={() => <List.Icon icon="comment-question" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+          <List.Item
+            title="Examination"
+            description="Physical examinations performed over the past visits"
+            left={() => <List.Icon icon="test-tube" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+           <List.Item
+            title="Medicine"
+            description="Medicine prescribed to the patient"
+            left={() => <List.Icon icon="pill" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+           <List.Item
+            title="Doctor"
+            description="Doctor registration"
+            left={() => <List.Icon icon="doctor" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+          <List.Item
+            title="Invitation link"
+            description="Use QR code to add friend"
+            left={() => <List.Icon icon="qrcode" />}
+            right={() => <List.Icon icon="chevron-right" />}
+            onPress={() => {}}
+          />
+        </List.Section>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  profile: {
+    alignSelf: 'center',
+    paddingVertical: 14,
+    flexDirection: 'row',
+    paddingHorizontal: 24,
+    textAlign: 'center'
+  },
+  settingCenter: {
+    paddingLeft:20,
+  },
+  editContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  patientName: {
+    fontSize: 18,
+    fontWeight: '800'
+  },
+  img: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    borderRadius: 80,
+  },
+  patientInfo: {
+    paddingHorizontal: 10,
+  }
+
+});
