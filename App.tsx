77917056acafb0e3b7755c6b65170c629b7e9a07
NguyenTran153 <nguyentran.07052002ak@gmail.com>
2024-04-13 16:54:24 +0700

    add theme

--- App.tsx
+++ App.tsx
@@ -3,10 +3,30 @@ import {MD3DarkTheme, MD3LightTheme, PaperProvider} from 'react-native-paper';
 import {useColorScheme} from 'react-native';
 
 import RootNavigator from './source/navigator/RootNavigator';
+import {createThemeFromSystemSchemes} from './source/utils/createTheme';
 
 const App = () => {
   const colorScheme = useColorScheme();
-  const paperTheme = colorScheme === 'dark' ? MD3DarkTheme : MD3LightTheme;
+
+  let primary = '#1976d2';
+
+  const systemSchemes: any = {
+    light: {
+      primary: primary,
+    },
+    dark: {
+      primary: primary,
+    },
+  };
+  const customTheme = createThemeFromSystemSchemes(systemSchemes);
+
+  const paperTheme =
+    colorScheme === 'dark'
+      ? {...MD3DarkTheme, colors: {...MD3DarkTheme.colors, ...customTheme.dark}}
+      : {
+          ...MD3LightTheme,
+          colors: {...MD3LightTheme.colors, ...customTheme.light},
+        };
 
   return (
     <PaperProvider theme={paperTheme}>
